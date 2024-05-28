0da28ea37a16dd8a4c8e2cc12e25e63db0dd52db
Mohammad Rahat <86203884+developerrahat@users.noreply.github.com>
2024-05-29 04:23:02 +0600

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -2,7 +2,7 @@
 // owl carousel start======================
 $('.brand_logos').owlCarousel({
   loop: true,
-  margin: 10,
+  margin: 2,
   autoplay: true,
   autoplaySpeed: 3000,
   responsiveClass: true,
@@ -21,3 +21,22 @@ $('.brand_logos').owlCarousel({
     }
   }
 });
+
+
+
+//Wow content animate=====================
+var wow = new WOW(
+  {
+    boxClass: 'wow',      // animated element css class (default is wow)
+    animateClass: 'animated', // animation css class (default is animated)
+    offset: 0,          // distance to the element when triggering the animation (default is 0)
+    mobile: true,       // trigger animations on mobile devices (default is true)
+    live: true,       // act on asynchronously loaded content (default is true)
+    callback: function (box) {
+      // the callback is fired every time an animation is started
+      // the argument that is passed in is the DOM node being animated
+    },
+    scrollContainer: null // optional scroll container selector, otherwise use window
+  }
+);
+wow.init();
